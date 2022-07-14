b195089f196d6c86415afbc2c9ae1b5e00f957db
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-07-15 00:04:30 +0500

    signin-ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
             <Routes>
                 <Route path='/signup' element={<SignUp />} />
+                <Route path='/signin' element={<SignIn />} />
             </Routes>
         </Router>
     </div>
